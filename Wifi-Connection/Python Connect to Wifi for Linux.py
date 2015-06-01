4669d7c8b504c8a429d67340ce034f6f41058ae3
agus makmun <summon.agus@gmail.com>
2015-06-02 01:20:07 +0700

    Update Python Connect to Wifi for Linux.py

--- Wifi-Connection/Python Connect to Wifi for Linux.py
+++ Wifi-Connection/Python Connect to Wifi for Linux.py
@@ -11,12 +11,12 @@ import os, sys
 
 print "[+] Enter this some Option"
 print " 1. Active Wifi (up)\n 2. Down Wifi (down)\n 3. Exit"
-inp_up_down = raw_input("Enter choice number: ")
+inp_up_down = raw_input("[+] Enter choice number: ")
 if inp_up_down == '1':
     os.system("ifconfig wlan0 up")
-    print "Are you want connect to wifi?"
+    print " Are you want connect to wifi?"
     print " 1. Yes, Conncet\n 2. No, Exit"
-    inp_connect = raw_input("Enter your choice: ")
+    inp_connect = raw_input("[+] Enter your choice: ")
     if inp_connect == '1':
         os.system("iwlist wlan0 scan | grep ESSID")
         masuk = raw_input("Enter name of wifi (ex: @wifi.id): ")
