034c38c18121036360e1e4956d320dc088535137
agus makmun <summon.agus@gmail.com>
2015-06-22 02:27:15 +0700

    Create wvdial.py

---
+++ wvdial/wvdial.py
@@ -0,0 +1,72 @@
+"""
+1. Download Wvdial
+2. lsusb
+3. lsusb -v -d 12d1:1506   --> go to check idVendor and idProduct
+4. python wvdia.py
+"""
+
+"""
+Name             : Python Wvdial
+Created By       : Agus Makmun (Summon Agus)
+Blog             : bloggersmart.net - python.web.id
+License          : GNU GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+Documentation    : https://github.com/agusmakmun/Some-Examples-of-Simple-Python-Script/
+"""
+
+import os
+idVendor = '0x12d1'
+idProduct = '0x1506'
+path = '/etc/wvdial.conf'
+setting = """
+[Dialer indosat]
+Init1 = ATZ
+Init3 = AT+CGDCONT=1,"IP","internet"
+Modem Type = USB Modem
+ISDN = 0
+Baud = 460800
+New PPPD = yes
+Modem = /dev/ttyUSB0
+Phone = *99#
+Modem = /dev/ttyUSB0
+Username = off
+Password = off
+Stupid mode = 1
+Auto DNS = off
+"""
+
+
+class WvDial(object):
+    def _connect(self, idVendor, idProduct):
+        os.system("modprobe usbserial vendor="+idVendor+" product="+idProduct)
+        os.system("wvdial indosat")
+
+    def _setting(self):
+        op = open(path, 'w')
+        op.write(setting)
+        op.close()
+        
+    def _checkRoot(self):
+        if os.getuid() != 0:
+            print "[-] Please Access as root..!"
+            sys.exit()
+        else:
+            pass
+    
+
+if __name__ == "__main__":
+    mome = WvDial()
+    mome._checkRoot()
+    print "Pilihan Setting: \n 1. Connect\n 2. Setting"
+    inp_usr = raw_input("Masukkan Pilihan: ")
+    if inp_usr == '1':
+        mome._connect(idVendor, idProduct)
+
+    elif inp_usr == '2':
+        mome._setting()
+
+    else:
+        pass
+    
+
+
+
