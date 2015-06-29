f7151eb7f9593d5a0995ca9fa96399af44375a76
agus makmun <summon.agus@gmail.com>
2015-06-29 20:37:39 +0700

    Create first_image_url.py

---
+++ grabbing/first_image_url.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+
+data = """
+<div>
+    <div class="post">
+        <a title="Brass-plated door knob" href="http:URL-LINK">
+            <img alt="IMAGE ALTERNATE TEXT" src="http://bloggersmart.net/wp-content/uploads/2015/06/7-Kapal-Pesiar-Terbesar-di-Dunia.jpg" />
+            <span class="det"><em class="fl">3.87 dollars</em><em class="fr">Housewares</em></span>
+            <strong class="vtitle">Brass-plated door knob</strong>
+        </a>
+
+        <div class="desc"><p>Brass-plated door knob</p></div>
+    </div>
+    <div class="post">
+        <a title="Brass-plated door knob2" href="http:URL-LINK2">
+            <img src="IMAGE SOURCE LINK" alt="IMAGE ALTERNATE TEXT"/>
+            <span class="det"><em class="fl">410.25 dollars</em><em class="fr">Housewares</em></span>
+            <strong class="vtitle">Brass-plated door knob2</strong>
+        </a>
+
+        <div class="desc"><p>Brass-plated door knob2</p></div>
+    </div>
+</div>
+"""
+
+soup = BeautifulSoup(data)
+result = []
+for post in soup.select('div.post'):
+    all_links = soup.findAll('img', src=True)
+    first = all_links[0]
+    string = str(first)
+    split = string.split('"')
+    result.append(split[3])
+
+print result[0]
