eee35cf5c9859f844e16f5dd233c89f1be2c1c8b
agus makmun <summon.agus@gmail.com>
2015-08-24 18:10:50 +0700

    Create check ip and mac address.py

---
+++ network/check ip and mac address.py
@@ -0,0 +1,10 @@
+agaust@agaust:~$ python
+iPython 2.7.6 (default, Jun 22 2015, 18:00:18) 
+[GCC 4.8.2] on linux2
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import commands
+>>> commands.getoutput('ifconfig wlan0').split()[6][5:]
+'192.168.1.17'
+>>> commands.getoutput('ifconfig wlan0').split()[4][0:]
+'2d:c0:5b:d6:81:f0'
+>>> 
