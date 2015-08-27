b426cff53a7272ab94161ef6d4acbc3d65e8472c
agus makmun <summon.agus@gmail.com>
2015-08-27 13:23:54 +0700

    Update __main__.py

--- Stegano-Extract-and-Read-File-From-Image/Make-a-Backdoor/backdoor.zip/__main__.py
+++ Stegano-Extract-and-Read-File-From-Image/Make-a-Backdoor/backdoor.zip/__main__.py
@@ -1,4 +1,4 @@
 import datetime
 print datetime.datetime.now()
 print "Put your script of python here."
-print "This is Simple Backdoor in image file, the execute with python programming."
+print "This is Simple Backdoor in image file, then execute with python programming."
