2581ab73a7cedcfe6d64f77a578cb9caf66b1280
agus makmun <summon.agus@gmail.com>
2015-09-10 16:33:24 +0700

    Create facepy.py

---
+++ facebook/facepy.py
@@ -0,0 +1,14 @@
+"""
+https://www.facebook.com/settings?tab=applications&section=all   --> change to public access.
+http://facepy.readthedocs.org/en/latest/usage/graph-api.html     --> docs graph api.
+https://developers.facebook.com/tools/explorer/                  --> change to allow some premission.
+https://github.com/jgorset/facepy                                --> repository.
+"""
+
+from facepy import GraphAPI
+ACCESS_TOKEN = 'tokentokentokentoken'
+ID_GROUP_OR_PAGE = '1234567890'
+graph = GraphAPI(ACCESS_TOKEN)
+
+graph.post(ID_GROUP_OR_PAGE+'/feed', message='Test My Status on Group/Pages')
+graph.post('me/feed', message='Test My Status')
