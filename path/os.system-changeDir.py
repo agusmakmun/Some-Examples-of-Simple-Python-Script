16a98b827e6ae2df818c97eb9899a62a919a5261
agus makmun <summon.agus@gmail.com>
2015-10-18 12:43:35 +0700

    Create os.system-changeDir.py

---
+++ path/os.system-changeDir.py
@@ -0,0 +1,10 @@
+>>> import os
+>>> os.getcwd()      # Return the current working directory
+'C:\\Python26'
+>>> os.chdir('/server/accesslogs')   # Change current working directory
+>>> os.system('mkdir today')   # Run the command mkdir in the system shell
+0
+
+#https://docs.python.org/2/tutorial/stdlib.html#operating-system-interface
+#http://stackoverflow.com/a/8248430
+#https://mail.python.org/pipermail/tutor/2012-February/088267.html
