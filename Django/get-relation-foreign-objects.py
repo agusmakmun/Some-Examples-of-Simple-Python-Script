b6bae6758f51abb0e066e64036e58db05fe7e48e
agus makmun <summon.agus@gmail.com>
2015-10-26 21:04:18 +0700

    Create get-relation-foreign-objects.py

---
+++ Django/get-relation-foreign-objects.py
@@ -0,0 +1,17 @@
+class Data_Toko(models.Model):
+  ....
+
+class Pengiriman(models.Model):
+	property = models.ForeignKey(Data_Toko, related_name='pengiriman')
+	metode_pengiriman = models.CharField(max_length=200, default='JNE', blank=True, null=True, help_text='Isikan Pengiriman yang tersedia, contoh: JNE')
+
+
+>>> a = Pengiriman.objects.all()
+>>> a
+[<Pengiriman: Pengiriman object>, <Pengiriman: Pengiriman object>]
+>>> 
+>>> from appearance.models import Pengiriman
+>>> a = Pengiriman.objects.all()
+>>> print ([p.metode_pengiriman for p in a])
+[u'Tiki', u'Pos Indonesia']
+>>> 
