d15cd043ed8020ca195440521f011efd47d7e1bf
agus makmun <summon.agus@gmail.com>
2015-10-26 13:35:54 +0700

    Update sites.py

--- Django/sites.py
+++ Django/sites.py
@@ -1,6 +1,13 @@
 #Error: Site matching query does not exist.
 #Solved: http://stackoverflow.com/a/16068551/3445802
 
+#settinsg.py
+SITE_ID = 1
+INSTALLED_APPS = (
+        ....
+        'django.contrib.sites',
+)
+
 $> ./manage.py shell
 >>> from django.contrib.sites.models import Site
 >>> site = Site()
