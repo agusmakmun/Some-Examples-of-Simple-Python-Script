e56c3be6dc3ab8bf31b7ce9a3d3db275b18207f0
agus makmun <summon.agus@gmail.com>
2015-10-26 13:42:29 +0700

    Create sql-all.py

---
+++ Django/sql-all.py
@@ -0,0 +1,8 @@
+$ ./manage.py sqlall name-app
+
+'''
+CommandError: App 'name-app' has migrations. 
+Only the sqlmigrate and sqlflush commands can be used when an app has migrations.
+'''
+
+So there before migrate to see it.
