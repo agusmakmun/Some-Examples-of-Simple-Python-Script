74141945527ce4d13dbe591af0c0f810b991e7e0
agus makmun <summon.agus@gmail.com>
2015-11-13 23:51:14 +0700

    Create Django email html.py

---
+++ Django/Django email html.py
@@ -0,0 +1,10 @@
+from django.core.mail import EmailMessage
+subject = "This is subject"
+message = "<html><head></head><body>This is body message</body></html>"
+
+def send_email(to_list, subject, message, sender="Aircourts <noreply@aircourts.com>"):
+    msg = EmailMessage(subject, message, sender, to_list)
+    msg.content_subtype = "html"  # Main content is now text/html
+    return msg.send()
+
+#Ref: http://masnun.com/2014/01/09/django-sending-html-only-email.html
