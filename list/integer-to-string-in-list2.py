92cdb41e49e953171d2b1e11c9197958c8918440
agus makmun <summon.agus@gmail.com>
2015-11-21 20:28:29 +0700

    Create integer-to-string-in-list2.py

---
+++ list/integer-to-string-in-list2.py
@@ -0,0 +1,57 @@
+def ganti_variable(inputan):
+    _split = inputan.split("\n")
+    for item in _split:
+        if item == '':
+            pass
+        else:
+            join = ' '.join(item)
+            mapping = map(int, join.split())
+            listing = [ p*"X" for p in mapping if p%2 != 0 and p > 2]
+            if len(listing) == 0:
+                pass
+            else:
+                char_replace = ["[", "]", ",", "'"]
+                print str(listing).translate(None, ''.join(char_replace))
+            
+inputan = """
+468557
+468647
+468719
+468841
+468913
+469099
+469229
+469331
+469439
+469589
+469717
+469811
+469939
+470081
+470179
+470263
+470389
+470461
+"""
+ganti_variable(inputan)
+
+
+
+#OUTPUT:
+XXXXX XXXXX XXXXXXX                                                                                                                                          
+XXXXXXX                                                                                                                                                      
+XXXXXXX XXXXXXXXX                                                                                                                                            
+XXXXXXXXX XXX                                                                                                                                                
+XXXXXXXXX XXXXXXXXX XXXXXXXXX                                                                                                                                
+XXXXXXXXX XXXXXXXXX                                                                                                                                          
+XXXXXXXXX XXX XXX                                                                                                                                            
+XXXXXXXXX XXX XXXXXXXXX                                                                                                                                      
+XXXXXXXXX XXXXX XXXXXXXXX                                                                                                                                    
+XXXXXXXXX XXXXXXX XXXXXXX                                                                                                                                    
+XXXXXXXXX                                                                                                                                                    
+XXXXXXXXX XXXXXXXXX XXX XXXXXXXXX                                                                                                                            
+XXXXXXX                                                                                                                                                      
+XXXXXXX XXXXXXX XXXXXXXXX                                                                                                                                    
+XXXXXXX XXX                                                                                                                                                  
+XXXXXXX XXX XXXXXXXXX                                                                                                                                        
+XXXXXXX
