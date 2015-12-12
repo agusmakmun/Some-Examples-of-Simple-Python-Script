b39690a2e232c8170791aeb36a80b7d7f0124f54
agus makmun <summon.agus@gmail.com>
2015-12-12 14:51:29 +0700

    Create understanding urls django.py
    function <code>get_absolute_url(self):</code> in <code>models.py</code> has reserve with name <code>page_detail</code> and with adding kwargs is <code>slug</code>. 
    So, in your <code>urls.py</code>, put name of <code>slug</code> from your <code>models.py</code> and with <code>name="page_detail"</code>

---
+++ Django/understanding urls django.py
@@ -0,0 +1,40 @@
+"""models.py"""
+class Page(models.Model):
+	nama = models.CharField(max_length=200)
+	slug = models.SlugField(max_length=200, unique=True)
+	publish = models.BooleanField(default=True, help_text='Status Publish Page.')
+	created = models.DateTimeField(auto_now_add=True)
+	body = RichTextUploadingField()
+
+	def __unicode__(self):
+		return self.nama
+
+	def get_absolute_url(self):
+		return reverse("page_detail", kwargs={"slug": self.slug})
+
+"""views.py"""
+class PageDetail(generic.DetailView):
+    model = models.Page
+    template_name = "pages.html"
+
+
+"""urls.py"""
+from . import views
+urlpatterns = [
+  ...,
+	url(r'^page/(?P<slug>\S+)/$', views.PageDetail.as_view(), name="page_detail"),
+]
+
+"""templates/page.html"""
+{% extends 'base.html' %}
+{% block content %}
+	<div class="page-detail">
+		<h2>
+			<a href="{% url "page_detail" slug=object.slug %}">{{ object.nama }}</a>
+		</h2>
+		<p class="meta-item">
+		  <span class="glyphicon glyphicon-calendar" aria-hidden="true"></span> {{ object.created }}
+		</p>
+		{{ object.body|safe }}
+	</div>
+{% endblock %}
