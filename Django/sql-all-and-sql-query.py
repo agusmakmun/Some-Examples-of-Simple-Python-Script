9f56d1925ceb84a288b942bb78088991d5f9bce5
agus makmun <summon.agus@gmail.com>
2016-03-01 19:06:30 +0700

    Update and rename sql-all.py to sql-all-and-sql-query.py

--- Django/sql-all-and-sql-query.py
+++ Django/sql-all-and-sql-query.py
@@ -6,3 +6,8 @@ Only the sqlmigrate and sqlflush commands can be used when an app has migrations
 '''
 
 So there before migrate to see it.
+
+>>> from app.models import Book
+>>> print Book.objects.all().query
+SELECT "app_book"."id", "app_book"."judul", "app_book"."slug", "app_book"."harga", "app_book"."detail" FROM "app_book"
+>>> 
