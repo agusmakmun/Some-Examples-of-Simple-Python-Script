226c3d7455122ccdaebf148e836b968e448d5b12
agus makmun <summon.agus@gmail.com>
2016-04-15 14:19:05 +0700

    Update views.py

--- Django/redirect/views.py
+++ Django/redirect/views.py
@@ -2,9 +2,6 @@ from django.http import HttpResponse
 from django.views.generic.base import RedirectView
 from django.core.urlresolvers import reverse
 
-def pool(request, pk):
-    return HttpResponse("You're looking at question %s." % pk)
-
 def pool_fix(request, pk):
     return HttpResponse("You're looking at question %s." % pk)
 
