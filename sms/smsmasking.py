d764e51b7d2503d645912a25df93fc1287c111de
agus makmun <summon.agus@gmail.com>
2016-06-30 00:12:18 +0700

    Create smsmasking.py

---
+++ sms/smsmasking.py
@@ -0,0 +1,31 @@
+import sys
+import json
+import requests
+
+URL_API      = 'http://smsmasking.ca/api.html'
+
+def sendSMS(sender, number, message):
+    message_data = {
+        'sender' : sender,
+        'number' : number,
+        'message': message
+    }
+    response    = requests.post(url=URL_API, data=message_data)
+    status_code = response.status_code
+    if status_code == 200:
+        print " {} to {}\n Message: {}".format(
+            json.loads(response.text)['success'],
+            number,
+            message
+        )
+    else:
+        print " Status Code: {}\n Info: {}".format(
+            status_code,
+            response.text
+        )
+
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print "\npython", __file__, "[sender] [number] [message]\n"
+        sys.exit()
+    sendSMS(sys.argv[1], sys.argv[2], sys.argv[3])
