6899bbe60d1f0af23cb6fe20207b4d5927ea099a
agus makmun <summon.agus@gmail.com>
2017-08-08 06:11:08 +0700

    Update auto_install_module.py

--- install/auto_install_module.py
+++ install/auto_install_module.py
@@ -5,6 +5,6 @@ try:
 except ImportError:
    import subprocess
    subprocess.call(['python', __file__.replace('scrap', 'install')])
-   from install import main as install_pip
+   from install import main as install_pip #.install.py
    install_pip()
    from bs4 import BeautifulSoup
