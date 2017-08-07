6b196e19829a7adbd7f300dbc66a80b31ac8dc92
agus makmun <summon.agus@gmail.com>
2017-08-08 06:07:38 +0700

    Create auto_install_module.py

---
+++ install/auto_install_module.py
@@ -0,0 +1,10 @@
+# Ref: https://github.com/rafi16jan/kurs-bi/blob/master/kurs_bi/scrap.py
+
+try:
+   from bs4 import BeautifulSoup
+except ImportError:
+   import subprocess
+   subprocess.call(['python', __file__.replace('scrap', 'install')])
+   from install import main as install_pip
+   install_pip()
+   from bs4 import BeautifulSoup
